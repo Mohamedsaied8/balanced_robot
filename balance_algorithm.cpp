0e92dedef85692439b2ed535a0efe2379c7246af
Mohamed Saied <infint_hero@hotmail.com>
2023-11-15 19:19:58 +0200

    complementry filter added

--- balance_algorithm.cpp
+++ balance_algorithm.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
-// design filter..
+// Implement complementry filter..
+template <class T>
+void ComplementryFilter(T roll, T pitch, T yaw)
+{
+
+}
 int main()
 {
+    double roll = 0.0, pitch = 0.0, yaw{0.0};
+    ComplementryFilter<double>(roll, pitch, yaw);
     return 0;
 }
